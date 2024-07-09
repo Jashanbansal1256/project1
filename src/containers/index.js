55b422d6a0951c6c62ceff5c87c0ac78c1d45a03
Jashan Bansal <jashanbansal7861@gmail.com>
2024-07-09 16:32:00 +0530

    create components and containers

---
+++ src/containers/index.js
@@ -0,0 +1,5 @@
+export {default as Blog} from './blog/Blog';
+export {default as Feature} from './feature/Feature';
+export {default as Footer} from './footer/Footer';
+export {default as Possibility} from './possibility/Possibility';
+export {default as WhatGpt3} from './whatgpt3/WhatGpt3';
---
+++ src/containers/whatgpt3/WhatGpt3.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './whatgpt3.css'
+
+
+const WhatGpt3 = () => {
+  return (
+    <div>WhatGpt3</div>
+  )
+}
+
+export default WhatGpt3
